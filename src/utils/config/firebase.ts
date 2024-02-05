6b23d7112fb571662f3fc97d0af7543c905d7fe5
cupid20103 <webphantom20103@gmail.com>
2024-02-06 07:06:39 +0900

    fix: updated the env setting

--- src/utils/config/firebase.ts
+++ src/utils/config/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbn7LvW-2GcDzNV9D4CcDfbzmQ5ramYtg",
-  authDomain: "cupid-c617f.firebaseapp.com",
-  projectId: "cupid-c617f",
-  storageBucket: "cupid-c617f.appspot.com",
-  messagingSenderId: "673887453566",
-  appId: "1:673887453566:web:c994326b14d08a5d18332f",
-  measurementId: "G-32XTBX2R49",
+  apiKey: process.env.EXPO_PUBLIC_EFIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default app;
+export { app, auth };
--- src/utils/functions/auth.ts
+++ src/utils/functions/auth.ts
@@ -1,29 +1,14 @@
-import { Alert, Platform } from "react-native";
-// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import app from "../config/firebase";
 import {
   PhoneFormDataType,
   SignInFormDataType,
   SignUpFormDataType,
 } from "@/types/auth";
 
-// const auth = getAuth(app);
-
 const signInWithUser = async (data: SignInFormDataType) => {};
 
 const signUpWithUser = async (data: SignUpFormDataType) => {};
 
-const signInWithGoogle = async () => {
-  // const provider = new GoogleAuthProvider();
-  // provider.setCustomParameters({
-  //   login_hint: "user@example.com",
-  // });
-  // const google_result: any = await signInWithPopup(auth, provider);
-  // if (Platform.OS !== "web") {
-  //   Alert.alert("Google", `${google_result}`);
-  // }
-  // console.log(google_result, "google");
-};
+const signInWithGoogle = async () => {};
 
 const signInWithFacebook = async () => {};
 
